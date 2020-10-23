c622bafb56988a4db29cc0181bfe260016fe6a69
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-10-23 13:00:44 +0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,14 +1,20 @@
 wget https://github.com/linkerd/linkerd2/releases/download/edge-20.10.4/linkerd2-cli-edge-20.10.4-linux-amd64 && mv -f linkerd2-cli-edge-20.10.4-linux-amd64 /usr/local/bin/linkerd && chmod +x /usr/local/bin/linkerd
 
 linkerd upgrade | kubectl apply --prune -l linkerd.io/control-plane-ns=linkerd -f -
-
 linkerd check
 linkerd version
 
 #NodePort
 k apply -n linkerd -f linkerdwebsvc.yml
+#linkerdwebdeploy disable host check
+k get deploy linkerd-web -n linkerd -oyaml > linkerdwebdeploy.yml
+nano linkerdwebdeploy.yml
+k -n linkerd apply -f linkerdwebdeploy.yml
+k rollout restart deployment.apps/linkerd-web -n linkerd
 
-linkerdwebdeploy disable host check
+k rollout restart deploy
+
+
 
 
 
