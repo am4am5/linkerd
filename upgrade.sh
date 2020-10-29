000a8fd6c421837e532c230c22bcbbc8f3c41536
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-10-29 17:18:42 +0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,19 +1,13 @@
-wget https://github.com/linkerd/linkerd2/releases/download/edge-20.10.5/linkerd2-cli-edge-20.10.5-linux-amd64 && mv -f linkerd2-cli-edge-20.10.5-linux-amd64 /usr/local/bin/linkerd && chmod +x /usr/local/bin/linkerd && linkerd version
+wget https://github.com/linkerd/linkerd2/releases/download/edge-20.10.6/linkerd2-cli-edge-20.10.6-linux-amd64 && mv -f linkerd2-cli-edge-20.10.6-linux-amd64 /usr/local/bin/linkerd && chmod +x /usr/local/bin/linkerd && linkerd version
 
 linkerd upgrade | kubectl apply --prune -l linkerd.io/control-plane-ns=linkerd -f -
-linkerd check
-linkerd version
 
 #NodePort
 k apply -n linkerd -f linkerdwebsvc.yml
 #linkerdwebdeploy disable host check
-k get deploy linkerd-web -n linkerd -oyaml > linkerdwebdeploy.yml
-nano linkerdwebdeploy.yml
-k -n linkerd apply -f linkerdwebdeploy.yml
-k rollout restart deployment.apps/linkerd-web -n linkerd
+kubectl get deploy linkerd-web -n linkerd -oyaml | sed '/last-applied-configuration/d' | sed '/enforced-host/d' | kubectl apply -n linkerd -f - && kubectl rollout restart deployment.apps/linkerd-web -n linkerd
 
-k rollout restart deploy
-linkerd check --proxy
+linkerd check
 
 
 
