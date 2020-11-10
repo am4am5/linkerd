c0bb4137a2e08823bb0f9e7d48fd0c9ccc9da767
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-11-10 11:53:05 +0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,4 @@
-wget https://github.com/linkerd/linkerd2/releases/download/edge-20.10.6/linkerd2-cli-edge-20.10.6-linux-amd64 && mv -f linkerd2-cli-edge-20.10.6-linux-amd64 /usr/local/bin/linkerd && chmod +x /usr/local/bin/linkerd && linkerd version
+wget https://github.com/linkerd/linkerd2/releases/download/stable-2.9.0/linkerd2-cli-stable-2.9.0-linux-amd64 && mv -f linkerd2-cli-stable-2.9.0-linux-amd64 /usr/local/bin/linkerd && chmod +x /usr/local/bin/linkerd && linkerd version
 
 linkerd upgrade | kubectl apply --prune -l linkerd.io/control-plane-ns=linkerd -f -
 
